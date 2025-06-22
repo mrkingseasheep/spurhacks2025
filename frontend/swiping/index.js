2718e56de52bc09fb0e5e0fe2b3a8be820264b98
Xinyang Wang <mrxywang@gmail.com>
2025-06-21 22:58:07 -0400

    Animation

--- frontend/swiping/index.js
+++ frontend/swiping/index.js
@@ -1,9 +1,33 @@
+const sitePic = document.getElementById("sitePic");
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+
+nextBtn.addEventListener("click", () => {
+    sitePic.classList.remove("flyLeft");
+    if (sitePic.classList.contains("flyRight")) {
+        sitePic.classList.remove("flyRight");
+        console.log("removing animation");
+    } else {
+        sitePic.classList.add("flyRight");
+        console.log("started animation");
+    }
+});
+
+prevBtn.addEventListener("click", () => {
+    sitePic.classList.remove("flyRight");
+    if (sitePic.classList.contains("flyLeft")) {
+        sitePic.classList.remove("flyLeft");
+        console.log("removing animation");
+    } else {
+        sitePic.classList.add("flyLeft");
+        console.log("started animation");
+    }
+});
+
 function nextSite() {
-    const wrapper = document.getElementById("sitePic");
-    wrapper.style.backgroundImage = 'url("../../img/lake1.png")';
+    sitePic.style.backgroundImage = 'url("../../img/lake1.png")';
 }
 
 function prevSite() {
-    const wrapper = document.getElementById("sitePic");
-    wrapper.style.backgroundImage = 'url("../../img/weedtree.jpg")';
+    sitePic.style.backgroundImage = 'url("../../img/weedtree.jpg")';
 }
