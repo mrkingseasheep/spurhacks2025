74a9487af635dcdcaa7876f7ee4f42da254dc976
Xinyang Wang <mrxywang@gmail.com>
2025-06-21 21:54:42 -0400

    Prev and next image

---
+++ frontend/swiping/index.js
@@ -0,0 +1,9 @@
+function nextSite() {
+    const wrapper = document.getElementById("sitePic");
+    wrapper.style.backgroundImage = 'url("../../img/lake1.png")';
+}
+
+function prevSite() {
+    const wrapper = document.getElementById("sitePic");
+    wrapper.style.backgroundImage = 'url("../../img/weedtree.jpg")';
+}
